97b5250d17ad7d0ad7b1b4e89414c3228f0d0af8
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-03-14 11:48:27 +0100

    added homePage and LoginPage with just a h1 element and created the routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import './HomePage.css';
+
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <h1>Startsidan</h1>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import './LoginPage.css';
+
+const LoginPage: React.FC = () => {
+  return (
+    <>
+      <h1>Inloggningen</h1>
+    </>
+  );
+};
+
+export default LoginPage;
