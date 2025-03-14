51de0ae99cbde0c8704d1effc2426da472bc59ed
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-03-14 13:26:34 +0100

    added path for /register, /search and /details

--- src/routing.tsx
+++ src/routing.tsx
@@ -3,25 +3,24 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Layout from './components/Layout';
+import DetailsPage from './pages/DetailsPage';
+import SearchResultPage from './pages/SearchResultPage';
+import RegisterPage from './pages/RegisterPage';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout/>,
         children: [
-            {
-                path: '/',
-                element: <HomePage/>,
-               
-            },
-            {
-                path: '/login',
-                element: <LoginPage/>
-            } 
+
+            {path: '/', element: <HomePage/>},
+            {path: '/login', element: <LoginPage/>},
+            {path: '/register', element: <RegisterPage/>}, 
+            {path: '/search', element: <SearchResultPage/>}, 
+            {path: '/details', element: <DetailsPage/>} 
 
         ]
     }
-    
 
 ]);
 
