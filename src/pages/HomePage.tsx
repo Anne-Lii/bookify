ed6641f6cc5fbc4b1cfadcdc1a25eeca61ce676d
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-03-14 11:53:18 +0100

    added links with NavLink to HomePage for navigation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import './HomePage.css';
+import Header from '../components/Header';
 
 
 const HomePage: React.FC = () => {
   return (
     <>
+    <Header/>
       <h1>Startsidan</h1>
     </>
   );
