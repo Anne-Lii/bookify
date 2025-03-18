b4528354bbf4e3e281375bd3daf05346720e7c1c
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-03-18 12:08:29 +0100

    adjusted som css to fit better in mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import SearchBar from '../components/SearchBar';
+import './HomePage.css';
 
 const HomePage: React.FC = () => {
   return (
-    <>
+    <div className='searchbar-container'>
       <h1>Search</h1>
       <SearchBar/>
       
-    </>
+    </div>
   );
 };
 
